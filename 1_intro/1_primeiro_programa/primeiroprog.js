3b05768212470d260dbec10648e8e4091fc484a7
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-12-01 19:28:10 -0300

    Update primeiroprog.js

--- 1_intro/1_primeiro_programa/primeiroprog.js
+++ 1_intro/1_primeiro_programa/primeiroprog.js
@@ -1,1 +1,10 @@
 console.log("Hello Node (: !");
+// Tarefa 01
+// 1. Crie um novo projeto de Node.js;
+// 2. Crie um arquivo para a aplicação com o nome programa;
+// 3. No arquivo crie duas variáveis e imprima a soma delas;
+// 4. Execute o arquivo e verifique a resposta no terminal;
+const a = 10;
+const b = 5;
+
+console.log(a + b);
