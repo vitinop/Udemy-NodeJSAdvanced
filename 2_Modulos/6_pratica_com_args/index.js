3497d68f256b81d74ac47eb33a9667fc1e6f7279
Victor Luz <85527775+vitinop@users.noreply.github.com>
2021-12-03 16:22:04 -0300

    exercicios até a aula 26

---
+++ 2_Modulos/6_pratica_com_args/index.js
@@ -0,0 +1,14 @@
+const minimist = require("minimist");
+//externo
+const args = minimist(process.argv.slice(2));
+
+//interno
+ const soma = require ('./soma').soma
+
+ soma(2,2)
+
+
+ const a =parseInt(args['a'])
+ const b =parseInt(args['b'])
+
+ soma(a,b)
